eb05a3d244feca39680eff1f107122ec88a9fe7c
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-07 17:52:35 +0500

    change cors url

--- src/app.js
+++ src/app.js
@@ -8,8 +8,19 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ems-frontend-gamma-gray.vercel.app"
+];
+
 app.use(cors({
-    origin: "https://ems-frontend-gamma-gray.vercel.app",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
