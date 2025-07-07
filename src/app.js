ec0976e460b8cc3aaad6f8a166c0c56ad1a1c31f
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-07 17:19:32 +0500

    change cors url

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://4bc4f03e-6724-4328-9524-a9a1108eeb02-00-2lbhj86on536i.pike.replit.dev",
     credentials:true
 }));
 app.use("/api/v1/users",router);
