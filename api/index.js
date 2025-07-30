984b357f63f37662ee81848c85cc360bb4936db7
Smb2003 <99506103+Smb2003@users.noreply.github.com>
2025-07-30 19:02:40 +0500

    make changes in the api folder

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const serverless = require("serverless-http");
 const app = require("../src/app.js");
 
-console.log(serverless(app));
-module.exports.handler  = serverless(app);
+module.exports = serverless(app);
